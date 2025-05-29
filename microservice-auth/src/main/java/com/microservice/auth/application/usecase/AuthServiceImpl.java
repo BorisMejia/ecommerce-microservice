889eacbed8efd0ed074574c96e57050346121f9e
BorisMejia <borisfernandez916@gmail.com>
2025-05-28 23:45:36 -0500

    feat: se implementa el login del usuario

--- microservice-auth/src/main/java/com/microservice/auth/application/usecase/AuthServiceImpl.java
+++ microservice-auth/src/main/java/com/microservice/auth/application/usecase/AuthServiceImpl.java
@@ -3,6 +3,7 @@ package com.microservice.auth.application.usecase;
 import com.microservice.auth.application.service.AuthService;
 import com.microservice.auth.domain.model.User;
 import com.microservice.auth.domain.repository.UserRepository;
+import com.microservice.auth.shared.exception.InvalidCredentialsException;
 import com.microservice.auth.shared.exception.UserAlreadyExistsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -33,6 +34,13 @@ public class AuthServiceImpl implements AuthService{
 
     @Override
     public Mono<User> login(String email, String password) {
-        return null;
+        return userRepository.findByEmail(email)
+                .switchIfEmpty(Mono.error(new InvalidCredentialsException()))
+                .flatMap(user -> {
+                    if (!passwordEncoder.matches(password, user.getPassword())){
+                        return Mono.error(new InvalidCredentialsException());
+                    }
+                    return Mono.just(user);
+                });
     }
 }

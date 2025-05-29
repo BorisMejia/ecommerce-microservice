1ab10d8cad4fed2e501a27eb5556a69291c67a71
BorisMejia <borisfernandez916@gmail.com>
2025-05-28 23:12:38 -0500

    feat: se implementa el registro de usuario

--- microservice-auth/src/main/java/com/microservice/auth/application/usecase/AuthServiceImpl.java
+++ microservice-auth/src/main/java/com/microservice/auth/application/usecase/AuthServiceImpl.java
@@ -3,11 +3,16 @@ package com.microservice.auth.application.usecase;
 import com.microservice.auth.application.service.AuthService;
 import com.microservice.auth.domain.model.User;
 import com.microservice.auth.domain.repository.UserRepository;
+import com.microservice.auth.shared.exception.UserAlreadyExistsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
 @RequiredArgsConstructor
+@Service
 public class AuthServiceImpl implements AuthService{
 
     private final UserRepository userRepository;
@@ -16,7 +21,14 @@ public class AuthServiceImpl implements AuthService{
 
     @Override
     public Mono<User> registerUser(String userName, String email, String password) {
-        return null;
+        return userRepository.existsByEmail(email)
+                .flatMap(exists -> {
+                    if (exists) return Mono.error(new UserAlreadyExistsException(email));
+                    UUID userId = UUID.randomUUID();
+                    String paswordHash = passwordEncoder.encode(password);
+                    User user = new User(userId, userName, email, paswordHash);
+                    return userRepository.save(user);
+                });
     }
 
     @Override

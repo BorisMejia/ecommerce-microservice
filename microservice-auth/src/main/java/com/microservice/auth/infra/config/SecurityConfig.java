cd434045f0002e43c0e61749e0387accdc2c1a53
BorisMejia <borisfernandez916@gmail.com>
2025-05-30 23:31:38 -0500

    refactor: se modifica la implementacion de la seguridad

--- microservice-auth/src/main/java/com/microservice/auth/infra/config/SecurityConfig.java
+++ microservice-auth/src/main/java/com/microservice/auth/infra/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.microservice.auth.infra.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -11,18 +12,18 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity){
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity, JwtAuthenticationFilter jwtFilter){
         return httpSecurity
-                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
-                        .pathMatchers("/public/**").permitAll()
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/api/auth/**").permitAll()
                         .anyExchange().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults())
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .addFilterAt(jwtFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                 .build();
 
     }
